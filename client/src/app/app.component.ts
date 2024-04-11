56fc6eb60a58d88398dad38139494fc6310413a9
Konstantin <constantin.bakalov@gmail.com>
2024-04-11 23:14:56 +0200

    created first route on backend for video info

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environment/environment';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+interface VideoInfo {
+  title: string;
+  description: string;
+  thumbnails: { url: string; width: number; height: number }[];
+  formats: any[];
+  viewCount: string;
+  isPrivate: boolean;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule, HttpClientModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  public url: string | undefined = undefined;
+  public info: VideoInfo | undefined = undefined;
+
+  constructor(private httpClient: HttpClient) {}
+
+  onSubmit() {
+    this.httpClient.post<VideoInfo>(environment.serverUrl, { url: this.url }).subscribe((result) => (this.info = result));
+  }
+}
