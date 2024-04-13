8412e5d313f194f71c2f79e89942fca34f1137d2
Konstantin <constantin.bakalov@gmail.com>
2024-04-13 22:09:44 +0200

    added endpoint for testing deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,9 @@ app.use(cors());
 app.use(json());
 
 app.use(downloadRouter);
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'works ' });
+});
 
 const port = config.port;
 app.listen(port, () => console.log(`Listening on ${port}`));
