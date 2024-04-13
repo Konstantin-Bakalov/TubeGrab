6f5051073ea0d90a097dcc014890caae000b67dc
Konstantin <constantin.bakalov@gmail.com>
2024-04-13 22:45:52 +0200

    added default port

--- server/dist/config.js
+++ server/dist/config.js
@@ -4,6 +4,6 @@ exports.config = void 0;
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
 const config = {
-    port: process.env.SERVER_PORT,
+    port: process.env.SERVER_PORT || 8080,
 };
 exports.config = config;
